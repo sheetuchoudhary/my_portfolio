739391bd3911b9278d77c14a8c781a8d69a1b0d9
Sheetal <sheetal.choudhary@avantika.edu.in>
2025-02-16 17:40:52 +0530

    Updates

--- src/components/Ptext.js
+++
@@ -1,7 +0,0 @@
-import Card from 'react-bootstrap/Card';
-
-function Ptext({ text }) {
-  return <Card.Text>{text}</Card.Text>;
-}
-
-export default Ptext;
--- src/components/Ptitle.js
+++
@@ -1,7 +0,0 @@
-import Card from 'react-bootstrap/Card';
-
-function Ptitle({ title }) {
-  return <Card.Title>{title}</Card.Title>;
-}
-
-export default Ptitle;
